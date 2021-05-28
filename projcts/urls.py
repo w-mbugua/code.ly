5b8dc12771d90b5895a3bcf3551af290ac34bad8
JoyMbugua <wacumbugua@gmail.com>
2021-05-28 15:40:09 +0300

    added projects crud urls

--- projcts/urls.py
+++ projcts/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import ProjectListView
+from .views import (
+    ProjectListView,
+    ProjectDetailView,
+    ProjectEditView,
+    ProjectDeleteView,
+)
 
 urlpatterns = [
-    path('', ProjectListView.as_view(), name="project_list")
+    path('', ProjectListView.as_view(), name="project_list"),
+    path('<int:pk>/', ProjectDetailView.as_view(), name="project_details"),
+    path('<int:pk>/edit/', ProjectEditView.as_view(), name="project_edit"),
+    path('<int:pk>/delete/', ProjectDeleteView.as_view(), name="project_delete"),
 ]
--- projcts/views.py
+++ projcts/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import DeleteView, UpdateView
 
 from .models import Project
 
 class ProjectListView(ListView):
     model = Project
     template_name = 'projcts/projects_list.html'
+
+class ProjectDetailView(DetailView):
+    pass
+
+
+class ProjectEditView(UpdateView):
+    pass
+
+class ProjectDeleteView(DeleteView):
+    pass
