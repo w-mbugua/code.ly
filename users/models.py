54895ff0c0fe02ec2ba42eedee8c22b04b66b463
JoyMbugua <wacumbugua@gmail.com>
2021-05-28 11:04:19 +0300

    added settings config and starter templates

--- users/models.py
+++ users/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class Customuser(AbstractUser):
+class CustomUser(AbstractUser):
     location = models.CharField(max_length=50, null=True, blank=True)
     occupation = models.CharField(max_length=50, null=True, blank=True)
