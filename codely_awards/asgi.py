c6362a406ba0fa3f5a790e73543a3b99b9a5a960
JoyMbugua <wacumbugua@gmail.com>
2021-05-28 10:37:44 +0300

    initial folder structure

---
+++ codely_awards/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for codely_awards project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codely_awards.settings')
+
+application = get_asgi_application()
