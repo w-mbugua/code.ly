94095b662f60483401a8eb3a7cea6072fdd0f1fc
JoyMbugua <wacumbugua@gmail.com>
2021-05-28 11:40:59 +0300

    added authentication

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import home
+from .views import home, SignUpView
 
 urlpatterns = [
     path('', home, name='home'),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from .forms import CustomUserCreationForm
+from django.urls import reverse_lazy
 
 
 def home(request):
     return render(request, 'home.html')
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
