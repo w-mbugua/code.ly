8c3831afc7771f9c5516579ff9f73aa290b5b5bd
JoyMbugua <wacumbugua@gmail.com>
2021-05-28 10:49:06 +0300

    added a custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Customuser(AbstractUser):
+    location = models.CharField(max_length=50, null=True, blank=True)
+    occupation = models.CharField(max_length=50, null=True, blank=True)
