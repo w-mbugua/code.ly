2e7d06e63f8013c0ec85d3d46c823a6115a5f40e
JoyMbugua <wacumbugua@gmail.com>
2021-05-31 13:41:56 +0300

    added basic tests for project model

--- projcts/tests.py
+++ projcts/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Project
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class ProjectModelTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username='Joe', email='jdoe@anon.com', password='bananas')
+        self.project = Project.objects.create(title='project noir', description='testing project', link='noir.com', creator=self.user)
+    
+    def test_toString(self):
+        self.assertEqual(str(self.project), self.project.title)
+    
+    def test_project_details(self):
+        self.assertEqual(self.project.title, 'project noir')
+        self.assertEqual(self.project.description, 'testing project')
+        self.assertEqual(self.project.link, 'noir.com')
+        self.assertEqual(self.project.creator.username, 'Joe')
+
+
